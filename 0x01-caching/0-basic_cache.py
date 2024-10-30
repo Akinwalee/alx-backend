6536d2170982c6afaf20781c2919515b008236f2
Akinwalee <obatulafuad@gmail.com>
2024-10-30 16:24:05 +0000

    fixing bugs

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,13 +11,19 @@ class BasicCache(BaseCaching):
     - constants of the caching systems
     - where the data are stored (in a dictionary)
     """
+    def __init__(self):
+        """
+        Initialize
+        """
+        self.cache_data = {}
+        super().__init__(self)
 
     def put(self, key, item):
         """
         Add an item to the cache
         """
         if key and item:
-            self.cace_data[key] = item
+            self.cache_data[key] = item
 
     def get(self, key):
         """
@@ -26,4 +32,4 @@ class BasicCache(BaseCaching):
         if not key or key not in self.cache_data.keys():
             return (None)
 
-        return (self.cach_data[key])
+        return (self.cache_data[key])
